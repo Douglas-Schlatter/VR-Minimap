53ef7cf8a3d80906b60b29f084870b90b28659d5
Matheus Rodrigues <mat.rodf@gmail.com>
2024-11-21 03:56:38 -0300

    Map Moving

--- Assets/Scripts/MoveMap.cs
+++ Assets/Scripts/MoveMap.cs
@@ -6,21 +6,18 @@ public class MoveMap : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject player;
-	public float scale = 0.1f;
-
+	public float scale = 0.05f;
     public Transform targetTransform;
+	
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
         targetTransform = this.gameObject.transform;
-        targetTransform.position = player.transform.position;
-        //targetTransform.rotation = player.transform.rotation;
-        //this.gameObject.transform.rotation = player.transform.rotation;
+        targetTransform.position = new Vector3(player.transform.position.x, 0, player.transform.position.z) * -scale;
         this.gameObject.transform.localPosition = targetTransform.position;
 
     }
