3e9ef2fbef15c3e6fdebbe8c92810dd0da64218f
Douglas-Schlatter <douglas.ardenghi18@gmail.com>
2024-11-20 22:17:27 -0300

    Added script for player input

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour
+{
+    public float vel = 10;
+    public void moveFoward() 
+    { 
+    
+    }
+}
