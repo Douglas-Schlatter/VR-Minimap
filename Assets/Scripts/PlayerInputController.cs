e825ed9b1add5c06aea917cd392ebf4f5befab54
Douglas-Schlatter <douglas.ardenghi18@gmail.com>
2024-11-20 23:52:54 -0300

    Added Base Movement and removed teleport from left hand

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class PlayerInputController : MonoBehaviour
 {
-    public float vel = 10;
+    public float moveSpeed = 10;
+    public GameObject targetObjectToMove;
+
     public void moveFoward() 
-    { 
-    
+    {
+        Vector3 movement = new Vector3(1, 0, 1);
+        this.gameObject.transform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+    public void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, 0, z);
+        targetObjectToMove.gameObject.transform.Translate(movement * moveSpeed * Time.deltaTime);
     }
 }
