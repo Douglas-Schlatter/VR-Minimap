a443d56a3f3455ccaeedb7e142ab74d8fad00ad7
Douglas-Schlatter <douglas.ardenghi18@gmail.com>
2024-11-28 19:35:02 -0300

    Toggle Map funcionando

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -6,8 +6,9 @@ public class PlayerInputController : MonoBehaviour
 {
     public float moveSpeed = 3;
     public GameObject targetObjectToMove;
-    
-    public void moveFoward() 
+    public GameObject mapPrefab;
+
+    public void MoveFoward() 
     {
         Vector3 movement = new Vector3(1, 0, 1);
         this.gameObject.transform.Translate(movement * moveSpeed * Time.deltaTime);
@@ -21,5 +22,10 @@ public class PlayerInputController : MonoBehaviour
 		movement.y = 0;
 		movement = Vector3.Normalize(movement);
         targetObjectToMove.gameObject.transform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+
+    public void ToggleMap()
+    {
+        mapPrefab.SetActive(!mapPrefab.activeSelf);
     }
 }
