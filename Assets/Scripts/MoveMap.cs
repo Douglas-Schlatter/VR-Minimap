afc10f6dd2785a65a836b5910fff2247b8762d32
Douglas-Schlatter <douglas.ardenghi18@gmail.com>
2024-11-28 22:43:21 -0300

    Revert "WIP Rotation"
    This reverts commit a74a36b3fe6d3b9556afe2aba993ee34269dc126.

--- Assets/Scripts/MoveMap.cs
+++ Assets/Scripts/MoveMap.cs
@@ -7,9 +7,7 @@ public class MoveMap : MonoBehaviour
     // Start is called before the first frame update
     public GameObject player;
 	public float scale = 0.05f;
-	public GameObject playerMarker;
-	
-	Vector3 initialPos;
+    public Transform targetTransform;
 	
     void Start()
     {
@@ -18,11 +16,9 @@ public class MoveMap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.gameObject.transform.localPosition = new Vector3(player.transform.position.x, 0, player.transform.position.z) * -scale;
-		playerMarker.transform.localPosition = new Vector3(player.transform.position.x, 0, player.transform.position.z) * 1/2;
-		Vector3 playerRotation =  -player.transform.rotation.eulerAngles;
-		playerRotation.x = 0;
-		this.gameObject.transform.localRotation = Quaternion.Euler(playerRotation);
+        targetTransform = this.gameObject.transform;
+        targetTransform.position = new Vector3(player.transform.position.x, 0, player.transform.position.z) * -scale;
+        this.gameObject.transform.localPosition = targetTransform.position;
 
     }
 }
