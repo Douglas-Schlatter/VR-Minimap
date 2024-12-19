f50056afa9f912a7ca909e02bd8b921795541b55
Douglas-Schlatter <douglas.ardenghi18@gmail.com>
2024-12-18 22:36:16 -0300

    Inputs funcionando! Voltamos a sprint 3!

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 
 public class PlayerInputController : MonoBehaviour
 {
@@ -8,7 +10,20 @@ public class PlayerInputController : MonoBehaviour
     public GameObject targetObjectToMove;
     public GameObject mapObject;
     public bool matchRotation = true;
+    public XRController rController;
+    //Input Related
+    public InputActionReference inputToggleMap;
 
+    public InputActionReference inputToggleMatchRotation;
+
+    public void Awake()
+    {
+        inputToggleMap.action.Enable();
+        inputToggleMap.action.performed += ToggleMap;
+
+        inputToggleMatchRotation.action.Enable();
+        inputToggleMatchRotation.action.performed += ToggleMatchRotation;
+    }
     public void MoveFoward() 
     {
         Vector3 movement = new Vector3(1, 0, 1);
@@ -26,19 +41,46 @@ public class PlayerInputController : MonoBehaviour
         targetObjectToMove.gameObject.transform.Translate(movement * moveSpeed * Time.deltaTime);
         */
         // old mapObject.transform.localRotation = Quaternion.Euler(-this.gameObject.transform.rotation.eulerAngles);
+       
         if (matchRotation)
         {
             mapObject.transform.localRotation = Quaternion.Euler(new Vector3(0, -this.gameObject.transform.rotation.eulerAngles.y, 0));
         }
     }
 
-    public void ToggleMap()
+    public void ToggleMap(InputAction.CallbackContext context)
     {
         mapObject.SetActive(!mapObject.activeSelf);
     }
 
-    public void ToggleMatchRotation()
+    public void ToggleMatchRotation(InputAction.CallbackContext context)
     {
         matchRotation = !matchRotation;
+    }
+    public void OnDeviceChange(InputDevice device, InputDeviceChange change)
+    {
+        switch (change)
+        {
+
+            case InputDeviceChange.Disconnected:
+                inputToggleMap.action.Disable();
+                inputToggleMap.action.performed -= ToggleMap;
+
+
+                inputToggleMatchRotation.action.Disable();
+                inputToggleMatchRotation.action.performed -= ToggleMatchRotation;
+                break;
+            case InputDeviceChange.Reconnected:
+                inputToggleMap.action.Enable();
+                inputToggleMap.action.performed += ToggleMap;
+
+
+                inputToggleMatchRotation.action.Enable();
+                inputToggleMatchRotation.action.performed += ToggleMatchRotation;
+                break;
+            default:
+                break;
+        }
+
     }
 }
